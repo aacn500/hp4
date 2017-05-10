3631ca0481580d8112ca669f97fd090d958af9a2
Andrew Nowak <an8@sanger.ac.uk>
2017-05-10 15:09:43 +0000

    splice data from one fd to another

--- hp4.c
+++ hp4.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
@@ -6,20 +8,21 @@
 #include <unistd.h>
 
 #include <sys/epoll.h>
+#include <sys/types.h>
 
 volatile sig_atomic_t stop;
 
 typedef struct Pipe {
     int read_fd;
     int write_fd;
-} pipe_t;
+} Pipe;
 
 void handle_sigint(int signum) {
     stop = 1;
 }
 
-pipe_t* pipe_new(void) {
-    pipe_t* new_pipe = malloc(sizeof(*new_pipe));
+Pipe* pipe_new(void) {
+    Pipe* new_pipe = malloc(sizeof(*new_pipe));
 
     int fds[2] = {0, 0};
     if (pipe(fds) < 0) {
@@ -43,24 +46,29 @@ pipe_t* pipe_new(void) {
     return new_pipe;
 }
 
-int close_pipe(pipe_t* pipe_to_close) {
+int close_pipe(Pipe* pipe_to_close) {
     return close(pipe_to_close->read_fd) | close(pipe_to_close->write_fd);
 }
 
-/* Open a pipe; print name of write end to stdout, then epoll read end until
+/*
+ * Open a pipe; print name of write end to stdout, then epoll read end until
  * sigint, echoing data to stdout.
  */
 int main(int argc, char** argv) {
-    printf("Hi\n");
-    pipe_t* my_pipe = pipe_new();
-    if (my_pipe == NULL) {
+    fprintf(stderr, "Hi\n");
+    Pipe* in_pipe = pipe_new();
+    Pipe* out_pipe = pipe_new();
+    if (in_pipe == NULL || out_pipe == NULL) {
         perror("pipe_new");
         return 1;
     }
 
+    int my_pid = getpid();
+
     signal(SIGINT, handle_sigint);
 
-    printf("Write to fd %d now!\n", my_pipe->write_fd);
+    fprintf(stderr, "Write to fd /proc/%d/fd/%d and", my_pid, in_pipe->write_fd);
+    fprintf(stderr, " read from fd /proc/%d/fd/%d\n", my_pid, out_pipe->read_fd);
 
     // epolling written with inspiration from
     // https://banu.com/blog/2/how-to-use-epoll-a-complete-example-in-c/
@@ -73,45 +81,53 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    event.data.fd = my_pipe->read_fd;
+    event.data.fd = in_pipe->read_fd;
     event.events = EPOLLIN;
 
-    if (epoll_ctl(efd, EPOLL_CTL_ADD, my_pipe->read_fd, &event) < 0) {
+    if (epoll_ctl(efd, EPOLL_CTL_ADD, in_pipe->read_fd, &event) < 0) {
         perror("epoll_ctl");
         return 1;
     }
 
+    long bytes_passed = 0l;
+
     while(!stop) {
         int n = epoll_wait(efd, events, 3, -1);
         for (int i = 0; i < n; i++) {
-            if ((events[i].events & EPOLLERR) || (events[i].events & EPOLLHUP) || (!(events[i].events & EPOLLIN))) {
+            if ((events[i].events & EPOLLERR) ||
+                    (events[i].events & EPOLLHUP) ||
+                    (!(events[i].events & EPOLLIN))) {
                 fprintf(stderr, "epoll error\n");
                 close(events[i].data.fd);
                 continue;
             }
-            else if (my_pipe->read_fd == events[i].data.fd) {
-                char* read_buf = calloc(1024, sizeof(char));
-                if (read(my_pipe->read_fd, read_buf, 1024) < 0) {
+            else if (in_pipe->read_fd == events[i].data.fd) {
+                ssize_t bytes_spliced = splice(in_pipe->read_fd, NULL,
+                       out_pipe->write_fd, NULL, 1024, SPLICE_F_NONBLOCK);
+                if (bytes_spliced < 0) {
                     if (errno & EAGAIN) {
                         errno = 0;
                         continue;
                     }
                     else {
-                        perror("read");
+                        perror("splice");
                         return 1;
                     }
                 }
-                printf("%s", read_buf);
+                bytes_passed += (long)bytes_spliced;
             }
         }
     }
 
-    if (close_pipe(my_pipe)) {
+    if (close_pipe(in_pipe) || close_pipe(out_pipe)) {
         perror("close_pipe");
         return 1;
     }
-    free(my_pipe);
-    my_pipe = NULL;
-    printf("Bye\n");
+    fprintf(stderr, "Read and wrote %ld bytes\n", bytes_passed);
+    free(in_pipe);
+    in_pipe = NULL;
+    free(out_pipe);
+    out_pipe = NULL;
+    fprintf(stderr, "Bye\n");
     return 0;
 }
