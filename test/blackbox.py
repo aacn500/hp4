1d603428a51eb9fe13ecf392b54016a75a8d19e6
Andrew Nowak <an8@sanger.ac.uk>
2017-06-23 15:46:16 +0000

    Add more pexpect tests

--- test/blackbox.py
+++ test/blackbox.py
@@ -14,7 +14,6 @@ script_dir = os.path.dirname(script_path)
 
 
 def test_smallfile():
-    file_gen.generate_testfile(script_dir + "/data/smallfile.txt", 51)
     child = pexpect.spawn(script_dir + "/../src/hp4 -f " +
                           script_dir + "/data/smallfile.json")
     out = []
@@ -30,12 +29,9 @@ def test_smallfile():
             assert "Lorem ipsum dolor sit Amet, consectetur volutpAt." in line
 
     os.remove(script_dir + "/data/smallfile_A.txt")
-    os.remove(script_dir + "/data/smallfile.txt")
 
 
 def test_largefile():
-    file_gen.generate_largefile(script_dir + "/data/")
-
     child = pexpect.spawn(script_dir + "/../src/hp4 -f " +
                           script_dir + "/data/largefile.json")
     out = []
@@ -51,10 +47,42 @@ def test_largefile():
             assert "Lorem ipsum dolor sit Amet, consectetur volutpAt." in line
 
     os.remove(script_dir + "/data/largefile_A.txt")
-    os.remove(script_dir + "/data/largefile.txt")
+
+
+def test_split_data_large():
+    child = pexpect.spawn(script_dir + "/../src/hp4 -f " +
+                          script_dir + "/data/split_data_large.json")
+
+    out = []
+    for line in child:
+        out.append(json.loads(line.decode()))
+
+    assert out[-1]["cat-to-seda"] == 524288000
+    assert out[-1]["seda-to-savea"] == 524288000
+    assert out[-1]["cat-to-sedt"] == 524288000
+    assert out[-1]["sedt-to-savet"] == 524288000
+
+    with open(script_dir + "/data/split_data_large_A.txt", 'r') as f:
+        for line in f:
+            # All 'a's have been capitalised to 'A'.
+            assert "Lorem ipsum dolor sit Amet, consectetur volutpAt." in line
+
+    with open(script_dir + "/data/split_data_large_T.txt", 'r') as f:
+        for line in f:
+            # All 't's have been capitalised to 'T'.
+            assert "Lorem ipsum dolor siT ameT, consecTeTur voluTpaT." in line
+
+    os.remove(script_dir + "/data/split_data_large_A.txt")
+    os.remove(script_dir + "/data/split_data_large_T.txt")
 
 
 if __name__ == "__main__":
-    i = pytest.main(["-v", "-p no:cacheprovider", script_path])
+    file_gen.generate_largefile(script_dir + "/data/")
+    file_gen.generate_testfile(script_dir + "/data/smallfile.txt", 51)
+
+    i = pytest.main(["-v", script_path])
+
+    os.remove(script_dir + "/data/largefile.txt")
+    os.remove(script_dir + "/data/smallfile.txt")
 
     sys.exit(i)
